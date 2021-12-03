73b34ed5740fe2a9c3da351e9c303b37122f3d6e
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2021-12-03 18:57:44 +0100

    more consistent side-effect checking

--- lenses/fuzz/static/run-tests.js
+++ lenses/fuzz/static/run-tests.js
@@ -4,6 +4,8 @@ const clone = rfdc();
 
 const runItButton = document.getElementById("run-it-button");
 
+const nativeConsole = console;
+
 const testIt = (func, test, message) =>
   it(message, () => {
     if (test.throws) {
@@ -44,43 +46,15 @@ const testIt = (func, test, message) =>
       actual = thrown;
     }
 
-    // test the actual result against the expected
+    // test for side-effects if any args were reference-types
     console.log("-- -- -- -- -- -- -- -- --");
 
-    if (test.throws) {
-      if (didThrow) {
-        console.log(
-          `%cthrew:`,
-          "font-weight: bold;",
-          `${actual.name}: ${actual.message}`
-        );
-      } else {
-        console.log(`%creturned:`, "font-weight: bold;", actual);
-        expect(() => {}).toThrow(test.expect);
-      }
-
-      if (test.expect instanceof Error) {
-        expect(() => {
-          throw actual;
-        }).toThrow(test.expect.constructor);
-        expect(() => {
-          throw actual;
-        }).toThrow(test.expect.message);
-      } else {
-        expect(() => {
-          throw actual;
-        }).toThrow(test.expect);
-      }
-    } else {
-      console.log(`%creturned:`, "font-weight: bold;", actual);
-
-      expect(actual).toEqual(test.expect);
-    }
-
-    // test for side-effects if any args were reference-types
     if (test.argsClone) {
-      console.log("-- -- -- -- -- -- -- -- --");
-      for (let i = 0; i < test.argsClone.length; i++) {
+      const lengthToCheck =
+        test.argsClone.length > test.args.length
+          ? test.argsClone.length
+          : test.args.length;
+      for (let i = 0; i < lengthToCheck; i++) {
         if (
           typeof test.argsClone[i] !== "object" ||
           test.argsClone[i] === null
@@ -90,7 +64,7 @@ const testIt = (func, test, message) =>
         try {
           expect(test.args[i]).toEqual(test.argsClone[i]);
         } catch (err) {
-          console.log(
+          nativeConsole.log(
             `%cside-effect in arg ${i + 1}:\n`,
             "font-weight: bold;",
             test.args[i]
@@ -98,7 +72,45 @@ const testIt = (func, test, message) =>
           throw err;
         }
       }
-      console.log(`%cno side-effects`, "font-weight: bold;");
+    }
+
+    console.log(`%cno side-effects`, "font-weight: bold;");
+
+    // test the actual result against the expected
+    console.log("-- -- -- -- -- -- -- -- --");
+
+    try {
+      if (test.throws) {
+        if (didThrow) {
+          console.log(
+            `%cthrew:`,
+            "font-weight: bold;",
+            `${actual.name}: ${actual.message}`
+          );
+        } else {
+          console.log(`%creturned:`, "font-weight: bold;", actual);
+          expect(() => {}).toThrow(test.expect);
+        }
+
+        if (test.expect instanceof Error) {
+          expect(() => {
+            throw actual;
+          }).toThrow(test.expect.constructor);
+          expect(() => {
+            throw actual;
+          }).toThrow(test.expect.message);
+        } else {
+          expect(() => {
+            throw actual;
+          }).toThrow(test.expect);
+        }
+      } else {
+        console.log(`%creturned:`, "font-weight: bold;", actual);
+
+        expect(actual).toEqual(test.expect);
+      }
+    } catch (err) {
+      throw err;
     }
   });
 
